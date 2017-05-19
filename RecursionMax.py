3500249e702088e7719d2e6ce82bfbec56238c36
Anh Dinh <dinhkhoathoanganh@gmail.com>
2017-05-19 20:44:19 +0700

    recursion exercise Max number first attempt

---
+++ RecursionMax.py
@@ -0,0 +1,33 @@
+# RecursionMax.py
+# A program to take a list of numbers, separated by space, from a file .txt and find the maximum number
+# using recursion.
+
+def SeparateNum(inputfile):    
+    infile = open(inputfile,'r')
+    data = infile.read()
+    infile.close()
+    return data.split()
+
+def FindMax(numlist):
+    if len(numlist) == 1:
+        return int(numlist[0])
+    else:
+        maxnum = FindMax(numlist[1:])
+        if maxnum > int(numlist[0]):
+            return maxnum 
+        else:
+            return int(numlist[0])
+
+def main():
+    print('This program will find your maximum number.')
+    # Get the list
+    inputfile = input('Enter the file name containing the number list: ')
+    numlist = SeparateNum(inputfile)
+
+    #Find max
+    maxnum = FindMax(numlist)
+
+    #Print results
+    print('Maximum number:', maxnum)
+
+main()
